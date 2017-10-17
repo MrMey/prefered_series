4984ec69289f5b8f50c55b3025fb550798069edd
DESKTOP-I67N5LM\Mr_Mey <meynard.ro@gmail.com>
2017-10-17 17:24:22 +0200

    dynamic values from the database and the api

--- code/web.py
+++ code/web.py
@@ -6,6 +6,7 @@
 
 from flask import Flask,request,render_template
 import request_database
+import request_api
 import user
 import series
 
@@ -14,7 +15,7 @@ class WebSite(Flask):
         Flask.__init__(self,__name__)
         self.add_url_rule(rule = '/',endpoint = 'main',view_func = self.main)
         self.add_url_rule(rule = '/main',endpoint = 'main',view_func = self.main)
-        self.add_url_rule(rule = '/login',endpoint = 'login',view_func = self.login, methods=['GET', 'POST'])
+        self.add_url_rule(rule = '/login',endpoint = 'login',view_func = self.login, methods=['POST'])
         self.add_url_rule(rule = '/details',endpoint = 'details',view_func = self.details)
         self.add_url_rule(rule = '/details/<serie>',endpoint = 'details',view_func = self.details)
         self.add_url_rule(rule = '/search_serie',endpoint = 'search_serie',view_func = self.search_serie, methods=['POST'])
@@ -53,14 +54,47 @@ class WebSite(Flask):
 class Controler():
     def __init__(self):
         self.req_database = request_database.DataBase()
+        
+        self.add_user()
+        self.act_series()
+        
+    def add_user(self):
         self.user = user.User("paul",0)
+    
+    def act_series(self):
         self.user.series = self.req_database.select_series_from_user(self.user.user_id)
         
 class FullControler(WebSite,Controler):
     def __init__(self):
         Controler.__init__(self)
         WebSite.__init__(self)
+        
+    def main(self):
+        """ **routes**
+            '/'
+            '/main'
+        """ 
+        return(render_template('main.html',**{"series_list":self.user.series}))
 
+    def search_serie(self):
+        """ **routes**
+            '/search_serie'
+        """
+        if request.method == 'POST':
+            series_list = [request_api.RequestAPI.get_basics(request.form['serie'])]
+            return(render_template('search.html',series_list = series_list))
+        return(0)
+
+    def details(self, serie = ""):
+        """ **routes**
+            '/details'
+            '/details/<serie>'
+        """
+        if serie == "":
+            serie = "Veuillez choisir une serie"
+        series_details = request_api.RequestAPI.get_details(serie)
+        
+        return(render_template('details.html',serie_name = series_details[1]))
     
 if __name__ == '__main__':
     app = FullControler()
