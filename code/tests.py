6a7f24c92e89366af7d1fba339646c5260af258e
DESKTOP-I67N5LM\Mr_Mey <meynard.ro@gmail.com>
2017-10-26 15:23:25 +0200

    commit tests

---
+++ code/tests.py
@@ -0,0 +1,14 @@
+# Authors: Elodie Ikkache, Romain Meynard, Jessica Cohen
+#
+# version 1.0
+# -*- coding: utf-8 -*-
+
+import test_request_api as trapi
+import test_request_database as trdb
+
+test_api = trapi.MyTestCase()
+test_api.test_request_api()
+
+
+test_db = trdb.MyTestCase()
+test_db.test_request_database()
